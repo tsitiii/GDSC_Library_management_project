07ced45687df9271a133faffd77001554b39b361
ZahirSeid <zahirseid1@gmail.com>
2024-03-17 18:26:44 +0300

    some modification on web page

--- library_manage/account/static/account/responsive.js
+++ library_manage/account/static/account/responsive.js
@@ -1,5 +1,3 @@
-
-
 const form = document.querySelector('form');
 form.addEventListener('submit', (event) => {
     
@@ -17,17 +15,6 @@ form.addEventListener('submit', (event) => {
     }
 
 });
-
-const greeting = (name) => {
-
-    console.log("hello")
-
-    const greet = document.createElement('div');
-    greet.textContent = `Hello ${name} !!!`
-    return greet
-}
-const welcome = document.getElementsByTagName('h1')[0]
-welcome.appendChild(greeting('zion the mean😊🎉'))
 
 function validateForm() {
     var checkbox = document.getElementById('id_is_student');
